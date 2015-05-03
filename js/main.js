b7d166f261cda3ee4e346b2931d390c5461987da
Tay Yang Shun <tay.yang.shun@gmail.com>
2015-05-03 20:48:23 +0800

    Add FB like box

--- js/main.js
+++ js/main.js
@@ -4,18 +4,8 @@ app.controller('LeaderboardController', function ($scope, $http) {
 
   $http.get('/leaderboard/latest.json').success(function (data) {
     $scope.leaderboard = data;
-    setTimeout(function () {
-      $scope.loaded = true;
-      $('.leaderboard-table tbody tr').each(function (index) {
-        // console.log(row)
-        (function (delay, el) {
-          setTimeout(function () {
-            $(el).addClass('animated fadeInLeft');
-          }, delay);
-        })(index * 100, this);
-      });
-      $scope.$apply();
-    }, 0);
+    $scope.$apply();
+    $('.table-container').addClass('shown');
   });
 });
 

ce2b0c984b82cf1cd71257d46156e9135ed41e0a
Tay Yang Shun <tay.yang.shun@gmail.com>
2015-05-03 20:17:04 +0800

    Some design

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 var app = angular.module('LeaderboardApp', []);
 
 app.controller('LeaderboardController', function ($scope, $http) {
+
   $http.get('/leaderboard/latest.json').success(function (data) {
-    console.log(data);
     $scope.leaderboard = data;
+    setTimeout(function () {
+      $scope.loaded = true;
+      $('.leaderboard-table tbody tr').each(function (index) {
+        // console.log(row)
+        (function (delay, el) {
+          setTimeout(function () {
+            $(el).addClass('animated fadeInLeft');
+          }, delay);
+        })(index * 100, this);
+      });
+      $scope.$apply();
+    }, 0);
   });
+});
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
 });
